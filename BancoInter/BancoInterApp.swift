fb95bbc6f3eb454f775f400494bdbc8ba3d53e5b
Leonardo <fernandes.leo@live.com>
2022-05-03 08:27:59 -0300

    Initial Commit

---
+++ BancoInter/BancoInterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoInterApp.swift
+//  BancoInter
+//
+//  Created by Leonardo Fernandes on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BancoInterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
